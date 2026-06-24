135f6d6d5d557ae78ea1a67d3d58dd67209b5c04
zynnels <sauloalt1@gmail.com>
2026-06-24 14:46:28 +0100

    Update sitedata.js

--- sitedata.js
+++ sitedata.js
@@ -14,12 +14,12 @@ const siteData = {
   sobre: {
     titulo: "Quem somos?",
     historia: [
-      "Somos um clã de minecraft focado em PvP 1.9+ e evolução constante.",
+      "Somos um clã de minecraft focado em PvP/Build/Farm e evolução constante.",
       "Nossa missão é dominar o servidor com estratégia, amizade e muito treino.",
-      "Um clã de guerreiros, e muita resenha !."
+      "Uma clan em crescimento procurando pessoas para recrutar !."
     ],
     tags: ["⚔️ PvP", "🎮 Minecraft", "🤝 Amizade", "🚀 Evolução"],
-    imagemSobre: "imagens/cla.png"  // ← .PNG
+    imagemSobre: "imagens/cla.png"  // 
   },
 
   // ---------- FUNDADORES ----------
@@ -41,55 +41,103 @@ const siteData = {
       { nome: "Baraaus", cargo: "Sub Leader", foto: "imagens/baraaus.png" },
       { nome: "Inject7", cargo: "Sub Leader", foto: "imagens/inject.png" },
       { nome: "AkyraZ", cargo: "Sub Leader", foto: "imagens/akyra.png" },
-      { nome: "Membro 6", cargo: "Elite", foto: "" },
-      { nome: "Membro 7", cargo: "Elite", foto: "" },
-      { nome: "Membro 8", cargo: "Membro", foto: "" },
-      { nome: "Membro 9", cargo: "Aliado", foto: "" },
-      { nome: "Membro 10", cargo: "Membro", foto: "" },
-      { nome: "Membro 11", cargo: "Membro", foto: "" },
-      { nome: "Membro 12", cargo: "Membro", foto: "" },
-      { nome: "Membro 13", cargo: "Membro", foto: "" },
-      { nome: "Membro 14", cargo: "Membro", foto: "" },
-      { nome: "Membro 15", cargo: "Elite", foto: "" }
+      { nome: "EuCoringando_ ", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "papo_de_passafome", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "Sozyeon", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "selfisdemons", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "EoIcebbXz", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "Sementinha", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "VxKaua", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "SanioMT", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "Marconigamer13", cargo: "Alta Elite", foto: "elite.png" },
+      { nome: "JuliaFlyffy", cargo: "Elite", foto: "elite.png" }
+      { nome: "Zakai_", cargo: "Elite", foto: "elite.png" }
+      { nome: "Itachi_Uchiha", cargo: "Elite", foto: "elite.png" }
+      { nome: "The_Ghost", cargo: "Elite", foto: "elite.png" }
+      { nome: "snoopaoPJL", cargo: "Elite", foto: "elite.png" }
+      { nome: "GCplays_nn", cargo: "Elite", foto: "elite.png" }
+      { nome: "whitemask", cargo: "Elite", foto: "elite.png" }
+      { nome: "piGabriel", cargo: "Elite", foto: "elite.png" }
+      { nome: "El_Milobrzx", cargo: "Elite", foto: "elite.png" }
+      { nome: "edu2253", cargo: "Elite", foto: "elite.png" }
+      { nome: "Cachorro_Caramel", cargo: "Elite", foto: "elite.png" }
+      { nome: "Zuru", cargo: "Elite", foto: "elite.png" }
+      { nome: "Nexxiltyy", cargo: "Elite", foto: "elite.png" }
+      { nome: "Senhor_Fornalha", cargo: "Elite", foto: "elite.png" }
+      { nome: "Vexpelion", cargo: "Elite", foto: "elite.png" }
+      { nome: "Usuaria9036", cargo: "Elite", foto: "elite.png" }
+      { nome: "zyxkill", cargo: "Elite", foto: "elite.png" }
+      { nome: "perrinho2011", cargo: "Membro", foto: "teste.png" }
+      { nome: "Kishino", cargo: "Membro", foto: "teste.png" }
+      { nome: "AllefMT", cargo: "Membro", foto: "teste.png" }
+      { nome: "L7ZXWK", cargo: "Membro", foto: "teste.png" }
+      { nome: "TiaAmora", cargo: "Membro", foto: "teste.png" }
+      { nome: "gabrielgg24187", cargo: "Membro", foto: "teste.png" }
+      { nome: "Igorrpl", cargo: "Membro", foto: "teste.png" }
+      { nome: "LozinBW12", cargo: "Membro", foto: "teste.png" }
+      { nome: "Bolsonaro_22", cargo: "Membro", foto: "teste.png" }
+      { nome: "b10pietro", cargo: "Membro", foto: "teste.png" }
+      { nome: "Abusandinho", cargo: "Membro", foto: "teste.png" }
+      { nome: ".Uchina", cargo: "Membro", foto: "teste.png" }
+      { nome: ".Silvrex01", cargo: "Membro", foto: "teste.png" }
+      { nome: "Itctrem", cargo: "Membro", foto: "teste.png" }
+      { nome: "GodLipe7272", cargo: "Membro", foto: "teste.png" }
+      { nome: "GalaticosPlay1K", cargo: "Membro", foto: "teste.png" }
+      { nome: "Dol7hin", cargo: "Membro", foto: "teste.png" }
+      { nome: "DiegoBaam", cargo: "Membro", foto: "teste.png" }
+      { nome: "Lowfigth", cargo: "Membro", foto: "teste.png" }
+      { nome: "Loosla", cargo: "Membro", foto: "teste.png" }
+      { nome: "KAISER_26", cargo: "Membro", foto: "teste.png" }
+      { nome: "jeguemakechina", cargo: "Membro", foto: "teste.png" }
+      { nome: "JasonMorumbi", cargo: "Membro", foto: "teste.png" }
+      { nome: "Polar_gg", cargo: "Membro", foto: "teste.png" }
+      { nome: "Phzin_yt", cargo: "Membro", foto: "teste.png" }
+      { nome: "Pensamentox  ", cargo: "Membro", foto: "teste.png" }
+      { nome: "Sunyzinho", cargo: "Membro", foto: "teste.png" }
+      { nome: "naoseibobar", cargo: "Membro", foto: "teste.png" }
+      { nome: "Miguelboy008", cargo: "Membro", foto: "teste.png" }
+      { nome: "Sou_Home", cargo: "Membro", foto: "teste.png" }
+      { nome: "SnowpyOF", cargo: "Membro", foto: "teste.png" }
+      { nome: "Roxinho", cargo: "Membro", foto: "teste.png" }
+      { nome: "Radis_yt", cargo: "Membro", foto: "teste.png" }
+      { nome: "Zero15337", cargo: "Membro", foto: "teste.png" }
+      { nome: "zaza666", cargo: "Membro", foto: "teste.png" }
+      { nome: "Ywkuut", cargo: "Membro", foto: "teste.png" }
+      { nome: "ydemonblatant__", cargo: "Membro", foto: "teste.png" }
+      { nome: "Vynzxx", cargo: "Membro", foto: "teste.png" }
+      { nome: "Lirioarthur37", cargo: "Membro", foto: "teste.png" }
+      { nome: "zFel1pe", cargo: "Membro", foto: "teste.png" }
+      { nome: "Weembu", cargo: "Membro", foto: "teste.png" }
+      { nome: "kgzwPANTERA", cargo: "Membro", foto: "teste.png" }
+      { nome: "EusouBonito", cargo: "Tester", foto: "teste.png" }
+      { nome: "Bayo", cargo: "Tester", foto: "teste.png" }
+      { nome: "Tobiaspug000", cargo: "Tester", foto: "teste.png" }
+      { nome: "Soul_Hunter13", cargo: "Tester", foto: "teste.png" }
+      { nome: "maja0102", cargo: "Tester", foto: "teste.png" }
+      { nome: ".Lucan4441445", cargo: "Tester", foto: "teste.png" }
     ]
   },
 
   // ---------- MEMBRO DO MÊS ----------
   membroMes: {
-    titulo: "Destaque do Mês",
+    titulo: "Player do Mês",
     subtitulo: "O membro mais foda do mês!",
     mes: "Junho 2026",
     membro: {
-      nome: "Em breve",
-      cargo: "---",
-      foto: "",
-      motivo: "🏆 Aguardando definição do destaque do mês",
-      destaque: "🚀 Em breve"
+      nome: "Lootier",
+      cargo: "Leader",
+      foto: "uriel.png",
+      motivo: "🏆 Quem mais ajudou o clã a crescer",
+      destaque: "🚀 W Lootier !"
     }
-  },
-
-  // ---------- TIERS DE PVP ----------
-  tiers: {
-    titulo: "Tiers da ZYN",
-    subtitulo: "Nossos níveis no PvP",
-    lista: [
-      { tier: "HT4", nome: "High Tier 4", icone: "imagens/crystal.png", cor: "#0044cc", categoria: "MCTier" },
-      { tier: "LT3", nome: "Low Tier 3", icone: "imagens/sword.png", cor: "#0066ff", categoria: "MCTier" },
-      { tier: "LT4", nome: "Low Tier 4", icone: "imagens/axe.png", cor: "#3399ff", categoria: "MCTier" },
-      { tier: "Crystal", nome: "Crystal", icone: "imagens/dimacrystal.png", cor: "#00ccff", categoria: "SubTier" },
-      { tier: "Diamante", nome: "Diamante", icone: "imagens/diamante.png", cor: "#66ccff", categoria: "SubTier" },
-      { tier: "Ouro", nome: "Ouro", icone: "imagens/ouro.png", cor: "#99ddff", categoria: "SubTier" },
-      { tier: "Ferro", nome: "Ferro", icone: "imagens/ferro.png", cor: "#aaddff", categoria: "SubTier" },
-      { tier: "Pedra", nome: "Pedra", icone: "imagens/pedra.png", cor: "#bbddff", categoria: "SubTier" }
-    ]
   },
 
   // ---------- CONQUISTAS ----------
   conquistas: {
     titulo: "Conquistas da ZYN",
     itens: [
-      "🏆 Em busca das primeiras vitórias",
-      "⚔️ Preparando para a primeira LIGA DE CLÃS",
+      "🏆 Em busca da glória infinita !",
+      "⚔️ Preparando para ganhar a LIGA DE CLÃS",
       "🔥 Clã em evolução constante",
       "👑 Rumo ao topo"
     ]
@@ -124,8 +172,8 @@ const siteData = {
   contato: {
     titulo: "Quer entrar na ZYN?",
     texto: "Venha ser membro da ZYN ON TOP!",
-    discord: "https://discord.gg/JtxUMU3nmu",
-    instagram: "https://instagram.com/zynontop",
+    discord: "https://discord.gg/hWz7HAsuqS",
+    instagram: "https://instagram.com/thezynelsoficial",
     email: "zynontop@gmail.com",
     onlineCount: 93
   },
